2763d58732e5bc86bf656bc59a8eb3b1247bd95d
marcobonni <marco.bonni19@gmail.com>
2023-04-15 18:54:37 +0200

    1

---
+++ crypto.mjs
@@ -0,0 +1,6 @@
+import { randomInt } from "crypto"
+function randomNumber1_100(min, max) {
+    const Id = randomInt(min, max)
+    console.log(Id)
+}
+export default randomNumber1_100
---
+++ import.mjs
@@ -0,0 +1,3 @@
+import randomNumber1_100 from "./crypto.mjs"
+
+randomNumber1_100(101, 200)
