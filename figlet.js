b0a463bb0541c8dbdb2439186217485d39b26296
marcobonni <marco.bonni19@gmail.com>
2023-04-16 21:44:35 +0200

    1

--- figlet.js
+++ figlet.js
@@ -1,6 +1,8 @@
+
+import * as fs from "fs";
 var figlet = require("figlet");
 
-figlet("Hello World!!", function (err, data) {
+fs.writeFilefiglet("Hello World!!", function (err, data) {
   if (err) {
     console.log("Something went wrong...");
     console.dir(err);
